d4975848598195f2871b9b01ab8505ceaebaa4e4
Augusto Andres <gutoea@gmail.com>
2019-09-12 18:54:39 -0500

    JS Vars and Randomizer

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// variables for win, losses and the total
+var wins = 0;
+var losses = 0;
+var total = 0;
+
+
+//randomizer
+var rock1 = Math.floor(Math.random() *12) + 1;
+var rock2 = Math.floor(Math.random() *12) + 1;
+var rock3 = Math.floor(Math.random() *12) + 1;
+var rock4 = Math.floor(Math.random() *12) + 1;
+console.log(rock3)
