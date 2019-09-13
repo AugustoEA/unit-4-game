c49d4bd342b1311f1b0c6ef64cabdc8f583a4b72
Augusto Andres <gutoea@gmail.com>
2019-09-12 19:33:36 -0500

    Working on Listeners

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,13 +11,20 @@ var rock3 = Math.floor(Math.random() *12) + 1;
 var rock4 = Math.floor(Math.random() *12) + 1;
 // console.log(rock3)
 
+
+// Listener
 $(".one").on("click", function () {
     total = total + rock1;
+    $('#finalTotal').text(total);
     if (total == randomNumber) {
         wins++;
+        $("#wins").text(wins);
         alert("You're a winner!!!!!!!!");
         reset();
     } else if (total > randomNumber) {
+        losses++;
+        $("#losses").text(losses);
+        alert("You're a loser........");
         reset();
     }
 
