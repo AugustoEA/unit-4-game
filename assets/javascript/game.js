f0373b3c09ebd80dc4076c85bb8d8a63f3bd1c75
Augusto Andres <gutoea@gmail.com>
2019-09-12 19:07:33 -0500

    Click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,4 +9,15 @@ var rock1 = Math.floor(Math.random() *12) + 1;
 var rock2 = Math.floor(Math.random() *12) + 1;
 var rock3 = Math.floor(Math.random() *12) + 1;
 var rock4 = Math.floor(Math.random() *12) + 1;
-console.log(rock3)
+// console.log(rock3)
+
+$(".one").on("click", function () {
+    total = total + rock1;
+    if (total == randomNumber) {
+        wins++;
+        alert("You're a winner!!!!!!!!");
+        reset();
+    } else if (total > randomNumber) {
+        reset();
+    }
+
