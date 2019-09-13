46701f9052a0f2f636798a42a0ff44e09f1e98f6
Augusto Andres <gutoea@gmail.com>
2019-09-12 19:56:40 -0500

    Reseter and copied if/else to other buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,93 @@
-// variables for win, losses and the total
-var wins = 0;
-var losses = 0;
-var total = 0;
+//document ready function
+$(document).ready(function () {
+
+    // variables for win, losses and the total
+    var wins = 0;
+    var losses = 0;
+    var total = 0;
 
 
-//randomizer
-//var rock1 = Math.floor(Math.random() *12) + 1;
-//var rock2 = Math.floor(Math.random() *12) + 1;
-//var rock3 = Math.floor(Math.random() *12) + 1;
-//var rock4 = Math.floor(Math.random() *12) + 1;
-// console.log(rock3)
+    //randomizer
+    var rock1 = Math.floor(Math.random() *12) + 1;
+    var rock2 = Math.floor(Math.random() *12) + 1;
+    var rock3 = Math.floor(Math.random() *12) + 1;
+    var rock4 = Math.floor(Math.random() *12) + 1;
+    // console.log(rock3)
 
-var randomNumber = Math.floor(Math.random() * 102) + 19;
-console.log(randomNumber);
+    var randomNumber = Math.floor(Math.random() * 102) + 19;
+    $("#randomNumber").text(randomNumber);
 
+    //Reseter
+    function reset() {
+        randomNumber = Math.floor(Math.random() * 102) + 19;
 
+    // Listener
+    $(".one").on("click", function () {
+        total = total + rock1;
+        $('#finalTotal').text(total);
+        if (total == randomNumber) {
+            wins++;
+            $("#wins").text(wins);
+            alert("!!!WINNER!!!");
+            reset();
+        } else if (total > randomNumber) {
+            losses++;
+            $("#losses").text(losses);
+            alert("!!!LOSER!!!");
+            reset();
+        }
 
-// Listener
-$(".one").on("click", function () {
-    total = total + rock1;
-    $('#finalTotal').text(total);
-    if (total == randomNumber) {
-        wins++;
-        $("#wins").text(wins);
-        alert("You're a winner!!!!!!!!");
-        reset();
-    } else if (total > randomNumber) {
-        losses++;
-        $("#losses").text(losses);
-        alert("You're a loser........");
-        reset();
-    }
+    });
 
+    $(".two").on("click", function () {
+        total = total + rock2;
+        $('#finalTotal').text(total);
+        if (total == randomNumber) {
+            wins++;
+            $("#wins").text(wins);
+            alert("!!!WINNER!!!");
+            reset();
+        } else if (total > randomNumber) {
+            losses++;
+            $("#losses").text(losses);
+            alert("!!!LOSER!!!");
+            reset();
+        }
+
+    });
+
+    $(".three").on("click", function () {
+        total = total + rock3;
+        $('#finalTotal').text(total);
+        if (total == randomNumber) {
+            wins++;
+            $("#wins").text(wins);
+            alert("!!!WINNER!!!");
+            reset();
+        } else if (total > randomNumber) {
+            losses++;
+            $("#losses").text(losses);
+            alert("!!!LOSER!!!");
+            reset();
+        }
+
+    });
+
+    $(".four").on("click", function () {
+        total = total + rock4;
+        $('#finalTotal').text(total);
+        if (total == randomNumber) {
+            wins++;
+            $("#wins").text(wins);
+            alert("!!!WINNER!!!");
+            reset();
+        } else if (total > randomNumber) {
+            losses++;
+            $("#losses").text(losses);
+            alert("!!!LOSER!!!");
+            reset();
+        }
+
+    });
+
+});
